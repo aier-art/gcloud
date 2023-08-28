67212e7a0870832df4a6ad8b6eea720e549d8c46
xxai.art <xxai.art@gmail.com>
2023-08-28 21:12:00 +0800

    ♨

---
+++ gcloud/kaggle.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+DIR=$(realpath $0) && DIR=${DIR%/*}
+cd $DIR
+set -ex
+
+source ./env
+
+./unpack.conf.sh
+for script in down.*.sh; do
+  if [ -f "$script" ]; then
+    bash "$script"
+  fi
+done
+
+clip_dir=clip-runtime
+if [ ! -d "$clip_dir" ]; then
+  git clone --depth=1 https://github.com/xxai-art/$clip_dir.git
+fi
+
+cd $clip_dir
+direnv exec . ./kaggle.sh
--- kaggle.sh
+++ kaggle.sh
@@ -15,21 +15,3 @@ else
   cd gcloud
   git pull
 fi
-cd gcloud
-
-source ./env
-
-./unpack.conf.sh
-for script in down.*.sh; do
-  if [ -f "$script" ]; then
-    bash "$script"
-  fi
-done
-
-clip_dir=clip-runtime
-if [ ! -d "$clip_dir" ]; then
-  git clone --depth=1 https://github.com/xxai-art/$clip_dir.git
-fi
-
-cd $clip_dir
-direnv exec . ./kaggle.sh
