382c7fd67f42a7664192ab10cde1d54de801fa0d
xxai.art <xxai.art@gmail.com>
2023-08-28 20:56:17 +0800

    ♨

---
+++ kaggle.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+DIR=$(realpath $0) && DIR=${DIR%/*}
+cd $DIR
+set -ex
+export DEBIAN_FRONTEND=noninteractive
+apt-get install -y git-lfs git
+
+cd ~
+
+if [ ! -d "gcloud" ]; then
+  git clone --depth=1 https://github.com/aier-art/gcloud.git
+  cd gcloud
+else
+  cd gcloud
+  git pull
+fi
+cd gcloud
+./down.dll.sh
+./down.model.sh
+git clone --depth=1 https://github.com/xxai-art/clip-runtime.git
+cd clip-runtime
+./kaggle.sh
+
+
