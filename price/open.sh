3d8d1e5aca4238347e61017c04be3bc39132862a
xxai.art <xxai.art@gmail.com>
2023-08-28 16:45:01 +0800

    ♨

--- price/open.sh
+++ price/open.sh
@@ -24,5 +24,5 @@ gcloud compute instances create $name \
   --shielded-vtpm \
   --shielded-integrity-monitoring \
   --labels=goog-ec-src=vm_add-gcloud \
-  --metadata-from-file=$DIR/init.sh \
+  --metadata-from-file startup-script=$DIR/init.sh \
   --reservation-affinity=any && echo "$name opened"
