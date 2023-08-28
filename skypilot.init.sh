e2c704fbfae7ed6a4943aee72417759a2210ae91
xxai.art <xxai.art@gmail.com>
2023-08-28 12:00:21 +0800

    ♨

--- skypilot.init.sh
+++ skypilot.init.sh
@@ -6,6 +6,14 @@ set -ex
 
 pip install "skypilot[gcp]"
 pip install google-api-python-client
+
+if ! [ -x "$(command -v gcloud)" ]; then
+  apt-get install -y apt-transport-https ca-certificates gnupg curl
+  echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+  apt-get update && apt-get -y install google-cloud-cli
+fi
+
 gcloud init
 gcloud auth application-default login
 sky check
