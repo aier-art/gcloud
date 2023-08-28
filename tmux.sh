9fb676d44d8974d16cc3795249b2a8b93ae8622b
xxai.art <xxai.art@gmail.com>
2023-08-28 17:48:12 +0800

    ♨

--- tmux.sh
+++ tmux.sh
@@ -4,10 +4,10 @@ DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
 
 if ! [ -x "$(command -v tmuxp)" ]; then
-apt-get install -y tmuxp
+  apt-get install -y tmuxp
 fi
 
 session=clip
 
 tmux has-session -t $session 2>/dev/null && exec tmux new-session -A -s $session
-exec tmuxp load $DIR/tmux.clip.yml
+exec tmuxp load $DIR/.tmux.clip.yml
